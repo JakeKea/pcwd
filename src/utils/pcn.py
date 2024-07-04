5d4c07105439e763e2646a4ab3b2c6f2caf11138
Jake Kealey <jake.kealey@nhs.net>
2024-07-04 18:30:13 +0100

    Removed Detailed Staff Role from the pipeline to simplify processing and fixed aggregation code. Removed output csv debug code.

--- src/utils/pcn.py
+++ src/utils/pcn.py
@@ -19,12 +19,13 @@ def process_pcn(data, date_data, env):
     #Identify relevant columns
     pcn_cols = (
         ["PCN_CODE", "PCN_NAME", 
-         "STAFF_GROUP", "STAFF_ROLE", "DETAILED_STAFF_ROLE", "FTE"])
+         "STAFF_GROUP", "STAFF_ROLE", "FTE"])
 
     #Format data for output
     ##Filter to just the context and sr columns
     df_pcn_agg = df_pcn_ncl.loc[:, pcn_cols]
-    df_pcn_agg = df_pcn_agg.groupby(pcn_cols[:-1]).agg({"FTE":"sum"})
+    df_pcn_agg = df_pcn_agg.groupby(pcn_cols[:-1])["FTE"].sum().reset_index()
+
 
     #Rename columns and add date column
     df_pcn_output = df_pcn_agg.rename(
@@ -32,7 +33,6 @@ def process_pcn(data, date_data, env):
                  "PCN_NAME": "pcn_name",
                  "STAFF_GROUP": "staff_group",
                  "STAFF_ROLE": "staff_role",
-                 "DETAILED_STAFF_ROLE": "staff_role_detailed",
                  "FTE": "fte"})
     
     
@@ -40,9 +40,6 @@ def process_pcn(data, date_data, env):
     #Add date columns
     df_pcn_output["date_data"] = date_data
     df_pcn_output["date_extract"] = datetime.today().strftime("%Y-%m-%d")
-
-    df_pcn_output.to_csv("test_output.csv",mode="w")
-
 
     #Return data
     return df_pcn_output
