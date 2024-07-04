3e822dc443cd573cc42a3800b4dabaa16fbe8765
Jake Kealey <jake.kealey@nhs.net>
2024-07-04 18:28:55 +0100

    Removed Detailed Staff Role from the output table as that level of detail is not currently needed

--- docs/sql_tables/wf_pc_pcwd_pcn.sql
+++ docs/sql_tables/wf_pc_pcwd_pcn.sql
@@ -6,7 +6,7 @@ CREATE TABLE [Data_Lab_NCL_Dev].[JakeK].[wf_pc_pcwd_pcn] (
 	pcn_name NVARCHAR(80) NOT NULL,
 	staff_group NVARCHAR(40) NOT NULL,
 	staff_role NVARCHAR(100) NOT NULL,
-	staff_role_detailed NVARCHAR(120) NOT NULL,
+	--staff_role_detailed NVARCHAR(120) NOT NULL,
     fte FLOAT NOT NULL
-	PRIMARY KEY (date_data, pcn_code, staff_role_detailed)
+	PRIMARY KEY (date_data, pcn_code, staff_role)
 )
